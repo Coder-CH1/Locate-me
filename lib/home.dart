7352539ea745becc290832c19602a6e3694a8067
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-07 01:11:37 +0100

    added

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,7 +10,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late final TextEditingController _controller;
+  LatLng _currentLocation = const LatLng(9.0820, 8.6753);
+  String _searchQuery = '';
+
   @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
@@ -22,6 +32,7 @@ class _HomeState extends State<Home> {
                 height: 40,
                 width: MediaQuery.of(context).size.width/0.2,
                 child: TextField(
+                  controller: _controller,
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.black12,
